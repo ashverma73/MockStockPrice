4f4f1bf3269118c9760f1f49404346d6f5d65908
Ashish Verma <ashverma13@gmail.com>
2017-06-25 00:18:53 +0100

    Added unit test

---
+++ MockStockPriceTests/_DefaultTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockStockPrice;
+using System;
+using System.Collections.Generic;
+
+using MockStockPrice.DataClient;
+namespace MockStockPrice.Tests
+{
+    [TestClass()]
+    public class _DefaultTests
+    {
+       
+        private static StockData _stockdata =null;
+        
+
+       
+        [TestMethod()]
+        public void GetTickerListTest()
+        {
+            MockedDataClient mdc = new MockedDataClient(50);
+            _stockdata = mdc.Data;
+            Assert.IsNotNull(_stockdata);
+            Assert.IsTrue(_stockdata.PricingData.Value.Count > 0);
+        }
+    }
+}
